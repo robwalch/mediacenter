577d6a8937652a37f93cd1efaac3b2855fcbf7b1
Rob Walch <rwalch@gmail.com>
2012-10-26 02:58:41 -0400

    show video resolution (height "p") and url in extension page action popup

--- MediaCenter.chromeextension/popup.js
+++ MediaCenter.chromeextension/popup.js
@@ -1,1 +1,16 @@
-console.log("popup");
+document.addEventListener('DOMContentLoaded', function () {
+  chrome.tabs.getSelected(undefined, function(tab) {
+    chrome.tabs.sendMessage(tab.id, {}, function(response){
+      console.log("response", response);
+      var url = response.url;
+      var html = [
+      "<p>",
+      response.height + "p",
+      "</p><p>",
+      response.url,
+      "</p>"
+      ];
+      document.body.innerHTML = html.join("");
+    });
+  });
+});
